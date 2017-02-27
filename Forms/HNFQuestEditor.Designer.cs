7e1176c14fda3b188991cd42321c89ea84e0645c
ZeroLRS <ZeroLRS@gmail.com>
2017-02-27 18:05:36 -0500

    Inital project commit.

---
+++ Forms/HNFQuestEditor.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace HNFQuestEditor
+{
+    partial class frmHNFQE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmHNFQE
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(384, 562);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 600);
+            this.MinimumSize = new System.Drawing.Size(400, 600);
+            this.Name = "frmHNFQE";
+            this.Text = "HNF Quest Editor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
